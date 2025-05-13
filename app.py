3c5ebe921252b532d5cd0057403b3b6128d4545f
jayalakshmiii <jaya002006@gmail.com>
2025-05-13 19:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,7 @@ def add():
 
 # ✅ Add this block to run the server
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
